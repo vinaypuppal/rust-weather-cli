3f78e2144ebb05aa558ee595dbd43f74c4a39022
vinaypuppal <me@vinaypuppal.com>
2021-08-19 21:09:56 +0530

    CLI args basic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 fn main() {
-    let api_token = std::env::var("API_TOKEN").expect("expected there to be an API token");
-    dbg!(api_token)
+    let _api_token = std::env::var("API_TOKEN").expect("expected there to be an API token");
+    let mut arg_iterator = std::env::args();
+    arg_iterator.next();
+    let args: String = arg_iterator.collect();
+    dbg!(args);
 }
